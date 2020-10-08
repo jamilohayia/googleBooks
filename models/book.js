534a9a68f2691a786fd3a27ed09d028df7dcd3bf
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-08 07:35:14 -0400

    added strings

--- models/book.js
+++ models/book.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
+  description: String,
+  image: String,
+  link: String, 
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
