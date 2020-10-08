a3c9cdecdac11bf10fd7c65747a6dcbea3c1a96b
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-08 07:08:06 -0400

    made search functionality to the API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
 export default {
+  // search function to retrive the books
+  searchTitle: function(query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+  },
+
   // Gets all books
   getBooks: function() {
     return axios.get("/api/books");
   },
-  // Gets the book with the given id
+  // get the books with a given ID 
   getBook: function(id) {
     return axios.get("/api/books/" + id);
   },
-  // Deletes the book with the given id
+  // delete the books with a given ID 
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves a book to the database
+  // this code saves the book to a database 
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
   }
